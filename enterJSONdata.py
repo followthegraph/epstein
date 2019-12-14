cb51a9aa105811ea4bdc3ddc3f853fe1fb72269e
Jeremy <jeremy@mcgrawautomation.com>
2019-12-14 04:56:04 -0600

    improved presentation for origin and destination

--- enterJSONdata.py
+++ enterJSONdata.py
@@ -3,11 +3,13 @@ from pprint import pprint
 from shutil import copyfile
 from colorama import init, Fore, Style
 from prettytable import PrettyTable
+from os import system
 from lists import aircraftIDs, aircraftMakes, aircraftModels, origs_dests, neo4j_import_dir
 # TODO: alphabetize lists (just use sort)
 
 
 def main():
+    init()  # for colorama
     with open('flight_log.json') as f:
         logs = json.load(f)
     enterFlightLog(logs)
@@ -16,114 +18,131 @@ def main():
 
 def enterFlightLog(logs):
     # Input
-    while True:
-        # check if flight number already exists
-        # looks like there may be duplicate flight numbers, may have to check to make sure that the flight and aircraft id
-        # or ignore what appears to be maintenance flights with duplicate flight numbers
-        pprint("|-------------------------------------------------------------|")
-        flightNumber = input("Flight Number: ")
-        for i in range(len(logs)):
-            if flightNumber == logs[i]['flightNumber']:
-                passengers = logs[i]['passengers']
-                flightNumCheck = True
-                break
-            else:
-                flightNumCheck = False
-                continue
+    system('cls')
+    # while True:
+    #     # check if flight number already exists
+    #     # looks like there may be duplicate flight numbers, may have to check to make sure that the flight and aircraft id
+    #     # or ignore what appears to be maintenance flights with duplicate flight numbers
+    #     pprint("|-------------------------------------------------------------|")
+    #     flightNumber = input("Flight Number: ")
+    #     for i in range(len(logs)):
+    #         if flightNumber == logs[i]['flightNumber']:
+    #             passengers = logs[i]['passengers']
+    #             flightNumCheck = True
+    #             break
+    #         else:
+    #             flightNumCheck = False
+    #             continue
 
-        if len(flightNumber) > 0:
-            if not flightNumCheck:
-                pprint('Adding Flight Number ' + flightNumber)
-                break
-            else:
-                pprint('Flight Number ' + flightNumber + ' already exists.')
-                continue
-        else:
-            pprint('Flight number must not be blank.')
-            continue
+    #     if len(flightNumber) > 0:
+    #         if not flightNumCheck:
+    #             pprint('Adding Flight Number ' + flightNumber)
+    #             break
+    #         else:
+    #             pprint('Flight Number ' + flightNumber + ' already exists.')
+    #             continue
+    #     else:
+    #         pprint('Flight number must not be blank.')
+    #         continue
 
-    while True:
-        # shouldn't be prior to 1997-12-14
-        # shouldn't be after 2006-01-19 (appears to be latest date, but pages may be out of order)
-        pprint("|-------------------------------------------------------------|")
-        flightDate = input("Date of flight (yyyy-mm-dd): ")
-        break
+    # system('cls')
+    # while True:
+    #     # shouldn't be prior to 1997-12-14
+    #     # shouldn't be after 2006-01-19 (appears to be latest date, but pages may be out of order)
+    #     pprint("|-------------------------------------------------------------|")
+    #     flightDate = input("Date of flight (yyyy-mm-dd): ")
+    #     break
 
+    # system('cls')
+    # pprint("|-------------------------------------------------------------|")
+    # simulation = input("Simulation (True/False): ")
+    # # auto-answer aircraft ID if simulation = True
+    # system('cls')
+    # pprint("|-------------------------------------------------------------|")
+    # notes = input("Notes: ")
+
+    # # may want to order or group these in some way, or store in an array that is iterated and printed
+    # system('cls')
+    # pprint("|-------------------------------------------------------------|")
+    # pprint("Aircraft IDs:")
+    # for i, id in enumerate(aircraftIDs):
+    #     pprint(str(i) + ' - ' + str(id))
+    # aircraftID_choice = input("Aircraft ID: ")
+    # if int(aircraftID_choice)+1 == len(aircraftIDs):
+    #     aircraftID = input("Aircraft ID (Other): ")
+    # else:
+    #     aircraftID = aircraftIDs[int(aircraftID_choice)]
+
+    # system('cls')
+    # pprint("|-------------------------------------------------------------|")
+    # pprint("Aircraft Make:")
+    # for i, id in enumerate(aircraftMakes):
+    #     pprint(str(i) + ' - ' + str(id))
+    # aircraftMake_choice = input("Aircraft Make: ")
+    # if int(aircraftMake_choice)+1 == len(aircraftMakes):
+    #     aircraftMake = input("Aircraft Make (Other): ")
+    # else:
+    #     aircraftMake = aircraftMakes[int(aircraftMake_choice)]
+
+    # system('cls')
+    # pprint("|-------------------------------------------------------------|")
+    # pprint("Aircraft Model:")
+    # for i, id in enumerate(aircraftModels):
+    #     pprint(str(i) + ' - ' + str(id))
+    # aircraftModel_choice = input("Aircraft Model: ")
+    # if int(aircraftModel_choice)+1 == len(aircraftModels):
+    #     aircraftModel = input("Aircraft Model (Other): ")
+    # else:
+    #     aircraftModel = aircraftModels[int(aircraftModel_choice)]
+
+    system('cls')
     pprint("|-------------------------------------------------------------|")
-    simulation = input("Simulation (True/False): ")
-    # auto-answer aircraft ID if simulation = True
-    pprint("|-------------------------------------------------------------|")
-    notes = input("Notes: ")
+    originList = sectionTable(
+        "Origin", """       Choose the origin """, 'origs_dests', logs)
 
-    # may want to order or group these in some way, or store in an array that is iterated and printed
-    pprint("|-------------------------------------------------------------|")
-    pprint("Aircraft IDs:")
-    for i, id in enumerate(aircraftIDs):
-        pprint(str(i) + ' - ' + str(id))
-    aircraftID_choice = input("Aircraft ID: ")
-    if int(aircraftID_choice)+1 == len(aircraftIDs):
-        aircraftID = input("Aircraft ID (Other): ")
-    else:
-        aircraftID = aircraftIDs[int(aircraftID_choice)]
-
-    pprint("|-------------------------------------------------------------|")
-    pprint("Aircraft Make:")
-    for i, id in enumerate(aircraftMakes):
-        pprint(str(i) + ' - ' + str(id))
-    aircraftMake_choice = input("Aircraft Make: ")
-    if int(aircraftMake_choice)+1 == len(aircraftMakes):
-        aircraftMake = input("Aircraft Make (Other): ")
-    else:
-        aircraftMake = aircraftMakes[int(aircraftMake_choice)]
-
-    pprint("|-------------------------------------------------------------|")
-    pprint("Aircraft Model:")
-    for i, id in enumerate(aircraftModels):
-        pprint(str(i) + ' - ' + str(id))
-    aircraftModel_choice = input("Aircraft Model: ")
-    if int(aircraftModel_choice)+1 == len(aircraftModels):
-        aircraftModel = input("Aircraft Model (Other): ")
-    else:
-        aircraftModel = aircraftModels[int(aircraftModel_choice)]
-
-    pprint("|-------------------------------------------------------------|")
-    pprint("Origin:")
-    for i, id in enumerate(origs_dests):
-        pprint(str(i) + ' - ' + str(id))
     origin_choice = input("Origin: ")
     if int(origin_choice)+1 == len(origs_dests):
         origin = input("Origin (Other): ")
     else:
         origin = origs_dests[int(origin_choice)]
 
+    system('cls')
     pprint("|-------------------------------------------------------------|")
     pprint("Destination:")
-    for i, id in enumerate(origs_dests):
-        pprint(str(i) + ' - ' + str(id))
+    destinationList = sectionTable(
+        "Destination", """       Choose the destination """, 'origs_dests', logs)
+
     destination_choice = input("destination: ")
     if int(destination_choice)+1 == len(origs_dests):
         destination = input("Destination (Other): ")
     else:
         destination = origs_dests[int(destination_choice)]
 
+    system('cls')
     pilot = input("Pilot: ")
 
+    system('cls')
     passengers = []
     while True:
+
         # TODO: ability to edit awaiting passengers
         passengerList = sectionTable("Passengers", """       Choose the number corresponding to the passenger
                  OR enter a new passenger"
-               Type x to indicate no more passengers """, 'passengers', flight_logs)
-        passenger = input("Passenger (" + str(passengers) + "): ")
+               Type x to indicate no more passengers """, 'passengers', logs)
+        passenger = input("Passenger(s) (" + str(passengers) + "): ")
 
         if passenger.isnumeric() and int(passenger) < len(passengerList):
             passengers.append(passengerList[int(passenger)])
+            system('cls')
             continue
         elif len(passenger) > 0 and passenger not in passengerList and passenger != 'x':
             passengers.append(passenger)
+            system('cls')
             continue
         elif len(passenger) == 0:
-            warningMSG = Fore.RED + 'must be at least one passenger' + Style.RESET_ALL
+            warningMSG = Fore.RED + \
+                '   !!!!!       must be at least one passenger   !!!!!       ' + Style.RESET_ALL
+            system('cls')
             print(warningMSG)
             continue
         else:
@@ -161,17 +180,26 @@ def enterFlightLog(logs):
 
 def sectionTable(header, instructions, sectionKey, logs):
     print("|-------------------------------------------------------------|")
-    print("    " + header)
+    print(Fore.BLUE + "    " + header + Style.RESET_ALL)
     print("|-------------------------------------------------------------|")
     print(Fore.RED + instructions + Style.RESET_ALL)
     print("|-------------------------------------------------------------|")
-    optionList = getUniqueValuesForKey(logs, sectionKey)
+    if sectionKey == 'origs_dests':
+        # optionList = set()
+        # optionList = getUniqueValuesForKey(logs, 'origin')
+        # optionList += getUniqueValuesForKey(logs, 'destination')
+        # optionList = list(optionList)
+        optionList = list(origs_dests)
+    else:
+        optionList = getUniqueValuesForKey(logs, sectionKey)
+
     optionList.sort()
     line = []
     tbl = PrettyTable(header=False, hrules=True,
                       horizontal_char='_', junction_char='/')
     for i in range(len(optionList)):
-        line.append('[' + str(i) + '] - ' + optionList[i])
+        line.append('[' + str(i) + '] - ' + Fore.GREEN +
+                    optionList[i] + Style.RESET_ALL)
         if len(line) % 5 == 0:
             tbl.add_row(line)
             line = []
